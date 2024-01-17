d4d63315d50de1c6e3a3a1f01f986f9858b99fab
Ahmed <ahmed@rocketship.se>
2024-01-17 16:10:11 +0100

    V3

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World! V2'
+    return 'Hello, World! V3'
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=5000)
